837e4e783caa754dfcaa9741879cd849fcb9d3a5
Sonia <soniastefaniaiani@gmail.com>
2026-01-09 21:12:54 +0200

    v4

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'add-nojekyll',
+      closeBundle() {
+        const distPath = join(__dirname, 'dist');
+        writeFileSync(join(distPath, '.nojekyll'), '');
+      },
+    },
+  ],
   base: '/food-waste-2026/',
   server: {
     proxy: {
